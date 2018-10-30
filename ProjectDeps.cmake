c25b96d2921d30d311df79b65827924778dda95a
Thomas Van Bogaert <thomas.vanbogaert@student.uantwerpen.be>
2018-10-31 00:37:57 +0100

    Travis.

--- ProjectDeps.cmake
+++ ProjectDeps.cmake
@@ -10,6 +10,7 @@ FetchContent_Declare(
 FetchContent_GetProperties(Catch2)
 if (NOT Catch2_POPULATED)
     FetchContent_Populate(Catch2)
+    set(CATCH_BUILD_TESTING OFF CACHE BOOL "Build SelfTest project")
     add_subdirectory(${catch2_SOURCE_DIR} ${catch2_BINARY_DIR})
 endif ()
 
@@ -21,6 +22,7 @@ FetchContent_Declare(
 FetchContent_GetProperties(nlohmann_json)
 if (NOT nlohmann_json_POPULATED)
     FetchContent_Populate(nlohmann_json)
+    set(JSON_BuildTests OFF CACHE BOOL "Build the unit tests when BUILD_TESTING is enabled.")
     add_subdirectory(${nlohmann_json_SOURCE_DIR} ${nlohmann_json_BINARY_DIR})
 endif ()
 
