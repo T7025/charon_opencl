e81130f042d678f33fa333ca64da6fdc5cfd8746
Thomas Van Bogaert <thomas.vanbogaert@student.uantwerpen.be>
2018-12-11 00:20:25 +0100

    Start of tests for Universe.

--- test/UniverseTest.cpp
+++ test/UniverseTest.cpp
@@ -3,3 +3,61 @@
 //
 
 #include <catch2/catch.hpp>
+#include <nlohmann/json.hpp>
+#include <settings/Settings.hpp>
+#include <base/BodyGenerator.hpp>
+
+class SingleBodyGenerator : public BodyGenerator {
+public:
+    using BodyGenerator::BodyGenerator;
+
+    std::tuple<fp, Vec3<fp>, Vec3<fp>> getBody() override {
+        return {1, {0, 0, 0}, {1, 1, 1}};
+    }
+};
+
+SCENARIO("Test the position update function", "[universe]") {
+    GIVEN("For every possible universe configuration") {
+        std::vector<nlohmann::json> settings{nlohmann::json{}};
+
+        auto addSettings = [&settings](const std::string &name, auto ...values) {
+            auto addSettingsImpl = [&settings](auto &self, size_t size, const std::string &name, auto &&value,
+                                               auto &&...values) {
+                if constexpr(sizeof...(values) > 0) {
+                    self(self, size, name, values...);
+                }
+                for (unsigned i = 0; i < size; ++i) {
+                    auto tempSettings = settings[i];
+                    tempSettings[name] = value;
+                    settings[i + size * sizeof...(values)] = tempSettings;
+                }
+            };
+            const auto size = settings.size();
+            settings.resize(settings.size() * sizeof...(values));
+            addSettingsImpl(addSettingsImpl, size, name, values...);
+        };
+
+        addSettings("algorithm", "brute-force", "barnes-hut");
+        addSettings("platform", "cpu-single-thread", "cpu-multi-thread", "opencl");
+//        addSettings("numberOfBodies", 1);
+//        addSettings("bodyGeneratorType", )
+
+        Universe()
+
+
+
+    }
+    GIVEN("A json object containing parsable settings") {
+        nlohmann::json json;
+        json["resultsDir"] = "testRes";
+        json["numberOfBodies"] = 500;
+
+        WHEN("A Settings object is initialized with the json object") {
+            Settings settings{json};
+            THEN("The Settings object should reflect the configuration values in the json object") {
+                REQUIRE(settings.resultsDir == json["resultsDir"]);
+                REQUIRE(settings.numberOfBodies == json["numberOfBodies"]);
+            }
+        }
+    }
+}
